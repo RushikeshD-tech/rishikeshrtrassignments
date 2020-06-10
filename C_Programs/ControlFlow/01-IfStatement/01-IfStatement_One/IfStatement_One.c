732e98ced007eb453da8b7e0bbfb7a25be37afdc
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-10 20:59:47 +0530

    IfStatement_One.c

---
+++ C_Programs/ControlFlow/01-IfStatement/01-IfStatement_One/IfStatement_One.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(void)
+{
+	 //variable declarations
+	int a_RAD, b_RAD, p_RAD;
+
+	//code
+	a_RAD = 9;
+	b_RAD = 30;
+	p_RAD = 30;
+
+	printf("\n\n");
+
+	if (a_RAD < b_RAD)
+	{
+		printf("A Is Less Than B !!!\n\n");
+    }
+    
+	if (b_RAD != p_RAD)
+	{
+		printf("B Is NOT Equal To P !!!\n\n");
+	}
+	
+	printf("Both Comparisons Have Been Done !!!\n\n");
+
+	return(0);
+}
+
+
+
+
+
