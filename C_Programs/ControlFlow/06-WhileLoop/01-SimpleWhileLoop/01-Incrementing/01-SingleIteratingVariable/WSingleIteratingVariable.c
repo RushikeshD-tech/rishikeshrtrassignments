243ca0c299e3c0c5745c43b4b0164d99b8e54f2f
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-11 01:50:46 +0530

    WsingleIteratingVariable.c

---
+++ C_Programs/ControlFlow/06-WhileLoop/01-SimpleWhileLoop/01-Incrementing/01-SingleIteratingVariable/WSingleIteratingVariable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i_RAD;
+	printf("\n\n");
+
+	printf("Printing Digits 1 to 10: \n\n");
+
+	i_RAD = 1;
+	while (i_RAD <= 10)
+	{
+		printf("\t%d\n", i_RAD);
+
+	}
+	printf("\n\n");
+
+	return(0);
+
+}
+
