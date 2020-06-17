84cb137b8751e49eb9fc913e76602ce554f97089
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-17 19:23:01 +0530

    CallMethod.c

---
+++ C_Programs/Functions/02-UserDefinedFunctions/02-MethodsOfFunctionCall/03-ThirdMethodOfCall/CallMethod.c
@@ -0,0 +1,82 @@
+#include <stdio.h> 
+
+int main(int argc, char* argv[], char* envp[])
+{
+	
+	void Function_Country();
+	//code
+	Function_Country();
+	return(0);
+}
+void Function_Country(void) 
+{
+	
+	void Function_OfAMC(void);
+	// code
+	Function_OfAMC();
+	printf("\n\n");
+	printf("I live In India.");
+	printf("\n\n");
+}
+void Function_OfAMC(void) 
+{
+	
+	void Function_Surname(void);
+	// code
+	Function_Surname();
+	printf("\n\n");
+	printf("Of ASTROMEDICOMP");
+}
+void Function_Surname(void) 
+{
+	
+	void Function_MiddleName(void);
+	// code
+	Function_MiddleName();
+	printf("\n\n");
+	printf("Dumbre");
+}
+void Function_MiddleName(void) 
+{
+	
+	void Function_FirstName(void);
+	// code
+	Function_FirstName();
+	printf("\n\n");
+	printf("Arun");
+}
+void Function_FirstName(void)
+{
+	
+	void Function_Is(void);
+	// code
+	Function_Is();
+	printf("\n\n");
+	printf("Rishikesh");
+}
+void Function_Is(void) 
+{
+	
+	void Function_Name(void);
+	//code
+	Function_Name();
+	printf("\n\n");
+	printf("Is");
+}
+void Function_Name(void) 
+{
+
+	void Function_My(void);
+	//code
+	Function_My();
+	printf("\n\n");
+	printf("Name");
+}
+
+void Function_My(void)
+{
+	//code
+	printf("\n\n");
+	printf("My");
+}
+
