c034bdc05cff4fe856bd481c20fc82a70662c1c9
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-17 17:33:52 +0530

    DefinitionMethod_03

---
+++ C_Programs/Functions/02-UserDefinedFunctions/01-MethodsOfFunctionDefinition/03-ThirdMethodOfDefinition/DefinitionMethod_03.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	void MyAddition(int, int);
+
+	int a_RAD, b_RAD;
+
+	//code
+	print("\n\n");
+	printf("Enter Integer Value For 'A' : ");
+	scanf("%d", &a_RAD);
+
+	printf("\n\n");
+	printf("Enter Integer Value For 'B' : ");
+	scanf("%d", &b_RAD);
+
+	MyAddition(a_RAD, b_RAD);
+
+	return(0);
+}
+
+
+void MyAddition(int a_RAD, int b_RAD)
+{
+	int sum_RAD;
+
+	//code
+	sum_RAD = a_RAD + b_RAD;
+	printf("\n\n");
+	printf("Sum Of %d And %d\n\n", a_RAD, b_RAD, sum_RAD);
+
+}
+
