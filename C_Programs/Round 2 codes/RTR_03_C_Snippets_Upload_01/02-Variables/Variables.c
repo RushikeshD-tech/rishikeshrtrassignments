66bf0ea621ab5307d0baba3dadd16e891b07aa96
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-30 22:55:31 +0530

    variables.c

---
+++ C_Programs/Round 2 codes/RTR_03_C_Snippets_Upload_01/02-Variables/Variables.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // variable declarations
+    int i_RAD = 5;           // Assign Value to i (integer)
+    float f_RAD = 3.9f;      // Assign Value to f (float)
+    double d_RAD = 8.041997; // Assign Value to d (double)
+    char c_RAD = 'A';        // Assign Value to c as character 'P'
+    
+	//code
+    printf("\n\n");
+    
+    printf("\ni_RAD = %d", i_RAD);
+    printf("\nf_RAD = %f", f_RAD);
+    printf("\nd_RAD = %lf", d_RAD);
+    printf("\nc_RAD = %c", c_RAD);
+
+    printf("\n\n");
+
+    i_RAD = 43;         // Assign Value to i (integer)
+    f_RAD = 6.54f;      // Assign Value to f (float)
+    d_RAD = 26.1294;    // Assign Value to d (double)
+    c_RAD = 'P';        // Assign Value to c as character 'P'
+    
+    printf("\ni_RAD = %d", i_RAD);
+    printf("\nf_RAD = %f", f_RAD);
+    printf("\nd_RAD = %lf", d_RAD);
+    printf("\nc_RAD = %c", c_RAD);
+
+    printf("\n\n");
+
+    return(0);
+}
+
+
+
+
+
+
