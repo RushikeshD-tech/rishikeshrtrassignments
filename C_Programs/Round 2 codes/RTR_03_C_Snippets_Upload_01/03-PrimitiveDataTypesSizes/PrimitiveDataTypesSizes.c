d0d62ea97ebd39924eb65fb86547a811a910e6db
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-06-30 22:59:33 +0530

    PrimitiveDataTypesSizes.c

---
+++ C_Programs/Round 2 codes/RTR_03_C_Snippets_Upload_01/03-PrimitiveDataTypesSizes/PrimitiveDataTypesSizes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//code
+    printf("\n\n");
+    
+    printf("\nSize Of 'int'          = %ld bytes", sizeof(int));                //signed integer
+    printf("\nSize Of 'unsigned int' = %ld bytes", sizeof(unsigned int));       //unsigned integer
+    printf("\nSize Of 'long'         = %ld bytes", sizeof(long));               //singend long
+    printf("\nSize Of 'long long'    = %ld bytes", sizeof(long long));          //long long
+    printf("\nSize Of 'float'        = %ld bytes", sizeof(float));              // signed float
+    printf("\nSize Of 'double'       = %ld bytes", sizeof(double));             // signed double
+    printf("\nSize Of 'long double'  = %ld bytes", sizeof(long double));        // signed long double
+
+    printf("\n\n");
+
+    return(0);
+}
+
+
+
+
+
+
+
+
