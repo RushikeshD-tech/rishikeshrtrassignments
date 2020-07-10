93c1910dbe9d6dd1449a838c7b9e39af52a0a866
Rhishikesh dumbre <meet2rhishi@gmail.com>
2020-07-10 15:53:31 +0530

    WindowFullscreen.cpp

---
+++ Windows/WindowFullscreen/WindowsFullscreen/WindowFullscreen.cpp
@@ -0,0 +1,155 @@
+// Headers
+#include<windows.h>
+
+// global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// global Variable declarations
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbFullscreen = false;
+HWND ghwnd = NULL;
+
+
+
+
+// WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	// code
+	// intialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// register above class
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	// message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	// local function declaration
+	void ToggleFullScreen(void);
+
+	// local variable declarations
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rc;
+	TCHAR str[] = TEXT("Hello World!!!");
+
+	switch (iMsg)
+	{
+	case WM_PAINT:
+		GetClientRect(hwnd, &rc);
+		hdc = BeginPaint(hwnd, &ps);
+		SetBkColor(hdc, RGB(0, 0, 0));
+		SetTextColor(hdc, RGB(0, 255, 0));
+		DrawText(hdc, str, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case 0x46:       // Capital 'F' hexadecimal value of 70
+		case 0x66:       // Small 'f' hexadecimal value of 102
+			ToggleFullScreen();
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+	void ToggleFullScreen(void)
+	{
+		// local Variable declaration
+		MONITORINFO mi{ sizeof(MONITORINFO) };
+
+		// code
+		if (gbFullscreen == false)
+		{
+			dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+			if (dwStyle & WS_OVERLAPPEDWINDOW)
+			{
+				if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+				{
+					SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+					SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+
+				}
+			}
+			ShowCursor(FALSE);
+			gbFullscreen = true;
+		}
+		else
+		{
+			SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+			SetWindowPlacement(ghwnd, &wpPrev);
+			SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+			ShowCursor(TRUE);
+			gbFullscreen = false;
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
